31f41be3b4b77cfb9fdf28b0f4f33f46dc9a27fc
hamstar0 <webutek@gmail.com>
2022-01-06 00:47:31 -0800

    v0.7.0 - Implemented water gauge

--- SteampunkArsenal/HUD/PressureGauge_Draw.cs
+++ SteampunkArsenal/HUD/PressureGauge_Draw.cs
@@ -11,14 +11,83 @@ using HUDElementsLib;
 namespace SteampunkArsenal.HUD {
 	partial class PressureGaugeHUD : HUDElement, ILoadable {
 		protected override bool PreDrawSelf( SpriteBatch sb ) {
+			Vector2 pos = this.GetHUDComputedPosition( true );
+
+			//
+
+			this.DrawGaugeBG( sb, pos );
+			this.DrawGaugeWater( sb, pos );
+			this.DrawGaugePin( sb, pos );
+
+			//
+
+			return false;
+		}
+
+
+		////////////////
+
+		private void DrawGaugeBG( SpriteBatch sb, Vector2 pos ) {
 			var mymod = SteamArseMod.Instance;
 			Texture2D bg = this.AnimState == 0
 				? mymod.GetTexture( "HUD/PressureGaugeBG_A" )
 				: mymod.GetTexture( "HUD/PressureGaugeBG_B" );
+
+			//
+
+			sb.Draw(
+				texture: bg,
+				position: pos,
+				color: Color.White
+			);
+		}
+
+
+		private void DrawGaugeWater( SpriteBatch sb, Vector2 pos ) {
+			var mymod = SteamArseMod.Instance;
+			Texture2D waterTex = mymod.GetTexture( "HUD/PressureGaugeWater" );
+
+			//
+
+			var myplayer = Main.LocalPlayer.GetModPlayer<SteamArsePlayer>();
+			float waterPerc = myplayer.MyBoiler.Water / myplayer.MyBoiler.Capacity;
+			if( waterPerc <= 0f ) {
+				return;
+			}
+
+			//
+
+			float waterTexSpanHeight = 50f;
+
+			int texHeight = (int)(waterPerc * waterTexSpanHeight);
+
+			float texOffsetY = 24f;
+			texOffsetY += (1f - waterPerc) * waterTexSpanHeight;
+
+			//
+
+			sb.Draw(
+				texture: waterTex,
+				position: pos + new Vector2(waterTex.Width, texOffsetY),
+				sourceRectangle: new Rectangle( 0, (int)texOffsetY, waterTex.Width, texHeight ),
+				color: Color.White * 0.25f,
+				rotation: 0f,
+				origin: default,
+				scale: 1f,
+				effects: SpriteEffects.None,
+				layerDepth: 0f
+			);
+		}
+
+
+		private void DrawGaugePin( SpriteBatch sb, Vector2 pos ) {
+			var mymod = SteamArseMod.Instance;
 			Texture2D fg = mymod.GetTexture( "HUD/PressureGaugeFG" );
 			Texture2D pin = mymod.GetTexture( "HUD/PressureGaugePin" );
 
-			Vector2 pos = this.GetHUDComputedPosition( true );
+			//
+
+			Vector2 pinOffset = new Vector2(fg.Width, fg.Height) * 0.5f;
 			Vector2 pinOrigin = new Vector2(
 				pin.Width / 2,
 				pin.Height / 2
@@ -35,14 +104,8 @@ namespace SteampunkArsenal.HUD {
 			//
 
 			sb.Draw(
-				texture: bg,
-				position: pos,
-				color: Color.White
-			);
-
-			sb.Draw(
 				texture: pin,
-				position: pos + new Vector2(bg.Width, bg.Height) * 0.5f,
+				position: pos + pinOffset,
 				sourceRectangle: null,
 				color: Color.White,
 				rotation: gauge,
@@ -52,15 +115,13 @@ namespace SteampunkArsenal.HUD {
 				layerDepth: 0f
 			);
 
+			//
+
 			sb.Draw(
 				texture: fg,
 				position: pos,
 				color: Color.White
 			);
-
-			//
-
-			return false;
 		}
 	}
 }
